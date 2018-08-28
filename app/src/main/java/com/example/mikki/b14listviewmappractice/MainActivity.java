6223521d7411ce2b15d1ba30f1f8d428be8e8dbc
mikkiGuo <mikkiguo29@yahoo.com>
2018-08-28 14:02:41 -0500

    initial commits

---
+++ app/src/main/java/com/example/mikki/b14listviewmappractice/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mikki.b14listviewmappractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] countries = {"China", "Japan", "USA", "India", "France", "Italy"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
